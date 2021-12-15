2e7051ccbbaf8bf3f83ef0786aa077b40b5c84a8
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2021-12-14 14:05:16 -1000

    Update indext.js

--- indext.js
+++ indext.js
@@ -38,7 +38,7 @@ function formatDate(date) {
      let  iconElement = document.querySelector("#icon");
      iconElement.setAttribute("src",
 `http://openweathermap.org/img/wn${response.data.weather[0].icon}@2x.png
-` );iconElement.setAttribute("alt", response.data.weather[0].description);
+` );
   }
   
   function searchCity(city) {
